16dce560b24bf3b9b973e2a2ee57cf1d6e77737d
Andres Aleman <alemanmartineza@gmail.com>
2019-05-11 14:09:45 -0500

    Se agregan comunicadores para notificar al front o a las pruebas de si el caso fue exitoso o erroneo

--- demo/core/src/main/java/com/movilmx/core/communication/MovieControllerNotifier.java
+++ demo/core/src/main/java/com/movilmx/core/communication/MovieControllerNotifier.java
@@ -1,4 +1,19 @@
 package com.movilmx.core.communication;
 
-public class MovieControllerNotifier {
+public interface MovieControllerNotifier {
+    public static final String TAG = MovieControllerNotifier.class.getSimpleName();
+
+    /**
+     * Cada evento que es completado en un controlador de perfil es notificado con esta interface
+     * @param eventType tipo de evento que fue completado
+     * @param movieControllerObject  objeto resultado de la operacion en el controlador.
+     */
+    void movieControllerEvent(MovieControllerEventType eventType, MovieControllerObject movieControllerObject);
+
+    enum MovieControllerEventType{
+        POPULAR, //Películas más populares
+        TOPRATED, //Películas top
+        UPCOMING, //Peliculas que se estrenarán
+        WARNING //Error
+    }
 }
--- demo/core/src/main/java/com/movilmx/core/communication/MovieControllerObject.java
+++ demo/core/src/main/java/com/movilmx/core/communication/MovieControllerObject.java
@@ -1,4 +1,64 @@
 package com.movilmx.core.communication;
 
 public class MovieControllerObject {
+    private int       code;
+    private String    msg;
+    private Object    data;
+    private Throwable exception;
+
+    public MovieControllerObject(){ }
+
+    public MovieControllerObject(Throwable exception){
+        this.exception = exception;
+    }
+
+    public MovieControllerObject(int code, String msg, Object data){
+        this.code = code;
+        this.msg  = msg;
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public MovieControllerObject setCode(int code) {
+        this.code = code;
+        return this;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public MovieControllerObject setMsg(String msg) {
+        this.msg = msg;
+        return this;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public MovieControllerObject setData(Object data) {
+        this.data = data;
+        return this;
+    }
+
+    public Throwable getException() {
+        return exception;
+    }
+
+    public void setException(Throwable exception) {
+        this.exception = exception;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieControllerObject{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", data=" + data +
+                '}';
+    }
 }
