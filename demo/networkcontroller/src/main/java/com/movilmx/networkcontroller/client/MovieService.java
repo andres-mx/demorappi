447429a363464d388a3a92f6c56a0b98556e6f74
Andres Aleman <alemanmartineza@gmail.com>
2019-05-12 00:06:37 -0500

    Se agrega llamada al nuevo servio de detalle de un video y tambien el response de dicho servicios

--- demo/networkcontroller/src/main/java/com/movilmx/networkcontroller/client/MovieService.java
+++ demo/networkcontroller/src/main/java/com/movilmx/networkcontroller/client/MovieService.java
@@ -1,11 +1,13 @@
 package com.movilmx.networkcontroller.client;
 
+import com.movilmx.networkcontroller.models.detailMovies.DetailMovie;
 import com.movilmx.networkcontroller.models.popular.Popular;
 import com.movilmx.networkcontroller.models.topRated.response.TopRated;
 import com.movilmx.networkcontroller.models.upComing.UpComing;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface MovieService {
@@ -17,4 +19,7 @@ public interface MovieService {
 
     @GET("movie/popular")
     Call<Popular> getPopular(@Query("page") String page, @Query("api_key") String apiKey);
+
+    @GET("movie/{movie_id}")
+    Call<DetailMovie> getDetailMovie(@Path("movie_id") String movieID, @Query("api_key") String apiKey);
 }
